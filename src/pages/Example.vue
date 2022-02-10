01dea64e06d68f37fd72e4e7d3df9d3791dfb00d
Andrey Gritsuk <axa8xa8@gmail.com>
2022-02-10 05:40:21 +0300

    init

---
+++ src/pages/Example.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="wrapper-content wrapper-content--fixed">
+    <section>
+      <div class="container">
+        <h1 class="title">Example page</h1>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
---
+++ src/pages/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="wrapper-content wrapper-content--fixed">
+    <section>
+      <div class="container">
+        <h1 class="title">Main Page</h1>
+      </div>
+    </section>
+  </div>
+</template>
